bcd6ea1554e00c0da06a00ac991527ad4c006ebb
李俊峰 <13816147439@163.com>
2019-07-21 22:23:42 +0800

    add basicModule

---
+++ BasicModule/BasicModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicModule
+//
+//  Created by lijunfeng on 2019/7/21.
+//  Copyright © 2019 lijunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
